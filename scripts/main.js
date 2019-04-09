0a5667fa7d4ed31fff19ff2b4cea39e4443e813a
9999starlight <40492494+9999starlight@users.noreply.github.com>
2019-04-09 18:37:48 +0200

    verzija3
    quiz added. html and scripts added

---
+++ scripts/main.js
@@ -0,0 +1,104 @@
+const search = document.querySelector('#search')
+search.addEventListener('keyup', pretragaFilm)
+const key = '94c8d066'
+const content = document.querySelector('.content')
+let filmIDS = []
+let filmId = ''
+const container = document.querySelector('.container')
+
+const closeModal = (element) => {
+  element.innerHTML = ''
+  close(element)
+}
+function pretragaFilm() {
+  // http://www.omdbapi.com/ je veza sa apijem. ?t=(da tražim po nazivu filma. u varijabli mi je value text polja. &apikey=${key} dodajem svoj api key) 
+
+  fetch(`http://www.omdbapi.com/?s=${search.value}&apikey=${key}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data)
+      console.log(data.Search)
+      let lista = ''
+      if (data.Search == undefined) {
+        lista = `<h3>Not found...</h3>`
+      } else {
+        data.Search.forEach(film => {
+          if (film.Poster == "N/A" || film.Poster.startsWith('http://'))
+            film.Poster = 'img/noimage.jpg'
+          filmId = film.imdbID
+          lista += `<div class = 'film flex shadow'>
+          <a href = 'https://www.imdb.com/title/${film.imdbID}' target = '_blank'><img src = '${film.Poster}' alt = 'film poster'/></a>
+          <div class = 'data flex'>
+          <h3>${film.Title}</h3><h5>Year: ${film.Year}</h5><h5>Type: ${film.Type}</h5>
+          <input type = 'button' onclick = 'prikazFilma("${filmId}")' class = 'infoBtn' value = 'More Info'>
+          </div></div>`
+          filmIDS.push(film.imdbID)
+        });
+      }
+      content.innerHTML = lista
+      console.log(filmIDS)
+    })
+  if (search.value === '') {
+    filmIDS = [] // da se isprazni niz kada je izbrisana prethodna pretraga
+  }
+}
+//let moreInfo
+let info = ''
+function prikazFilma(filmId) {
+  for (let i = 0; i < filmIDS.length; i++) {
+    if (filmIDS[i] == filmId) filmId = filmIDS[i]
+  }
+  fetch(`http://www.omdbapi.com/?i=${filmId}&apikey=${key}`)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.Response !== "False") {
+        if (data.Poster == "N/A" || data.Poster.startsWith('http://'))
+          data.Poster = 'img/noimage.jpg'
+        info = `<i class="fas fa-window-close" id = "movieX"></i>
+        <div class = 'film flex shadow'>
+        <div class = "info-poster flex shadow">
+        <div class = "poster shadow"><img src = '${data.Poster}' alt = 'film poster' /></div>
+        <div class = 'data flex'>
+        <h2>${data.Title}</h2>
+        <h3>Year: ${data.Year}</h3>
+        <h3>Type: ${data.Type}</h3>
+        <h3>Genre: ${data.Genre}</h3>
+       <h3>IMDB Ratings: ${data.imdbRating}</h3>
+       <button class = 'addMovieBtn'>Add to watchlist</button>
+       </div>
+       </div>
+       <h4>Actors: ${data.Actors}</h4><h4>Awards: ${data.Awards}</h4><h4>Runtime: ${data.Runtime}</h4><h4>Country: ${data.Country}</h4><h5>Plot: ${data.Plot}</h5><h2>Director: ${data.Director}</h2><a href = 'https://www.imdb.com/title/${data.imdbID}' target = '_blank'>IMDB</a></div>`
+      } else {
+        info = "Not found..."
+      }
+      const moreInfo = document.querySelector('.more-info')
+      moreInfo.innerHTML += info
+      open(moreInfo)
+      close(content)// da se ne vidi content odnosno onemogući biranje još modal info-a. dodaj neku boju ili nešto
+      document.querySelector('#movieX').addEventListener('click', function(){closeModal(moreInfo)
+    , open(content)});
+    })
+}
+
+//nekadašnji za zatvaranje ali ne valja
+/*
+      const closeModal = (e) => {
+        if (e.target.classList.contains('container') || e.target.classList.contains('fas')){
+          console.log(e.target)
+          moreInfo.innerHTML = ''
+          close(moreInfo)
+          open(content)// vrati display filmova
+      }
+    }
+    container.addEventListener('click', closeModal);
+    document.querySelector('#movieX').addEventListener('click', closeModal);
+
+
+
+
+    const closeModal = () => {
+  moreInfo.innerHTML = ''
+  close(moreInfo)
+  open(content)// vrati display filmova
+}
+*/
